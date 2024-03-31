b96d27dcaf69aa0c1089870d0b0f027f538021cc
Kevin Nathanael Halim <108170234+zetsux@users.noreply.github.com>
2024-03-31 09:50:50 +0000

    fix: adjust fun commands

--- fun/Pat.py
+++ fun/Pat.py
@@ -3,29 +3,32 @@ import json
 import urllib.request as urllib2
 from discord.ext import commands
 from discord import app_commands
-from discord.commands import Option
 
-guilds = [990445490401341511]
 
 class Pat(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-  @app_commands.command(name='pat', description='Do an anime patpat')
-  async def pat(self, ctx: discord.Interaction, member: Option(discord.Member, "The one you will patpat", required=False, default=None)):
-      response = urllib2.urlopen('https://some-random-api.ml/animu/pat')
-      data = json.loads(response.read())
-  
-      if member == None:
-          embed = discord.Embed()
-          embed.set_image(url=data['link'])
-  
-      else:
-          mentionUser = '<@' + str(member.id) + '>'
-          embed = discord.Embed(description=f"*patpat {mentionUser}*")
-          embed.set_image(url=data['link'])
-  
-      await ctx.response.send_message(embed=embed)
+    @app_commands.command(name='pat', description='Do an anime patpat')
+    @app_commands.describe(member='The member to pat')
+    async def pat(self, ctx: discord.Interaction, member: discord.Member = None):
+        try:
+            response = urllib2.urlopen('https://some-random-api.ml/animu/pat')
+            data = json.loads(response.read())
+
+            if member == None:
+                embed = discord.Embed(description=f"*patpat You*")
+            else:
+                mentionUser = '<@' + str(member.id) + '>'
+                embed = discord.Embed(description=f"*patpat {mentionUser}*")
+                embed.set_image(url=data['link'])
+            embed.set_image(url=data['link'])
+
+            await ctx.response.send_message(embed=embed)
+        except Exception as error:
+            await ctx.response.send_message("Yang bikin gif anime lg turu")
+            print(error)
+
 
 async def setup(bot):
-  await bot.add_cog(Pat(bot))
+    await bot.add_cog(Pat(bot))
--- fun/Pedo.py
+++ fun/Pedo.py
@@ -1,21 +1,25 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-from discord.commands import Option
 
-guilds = [990445490401341511]
 
 class Pedo(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-  @app_commands.command(name='pedo', description='Expose a lolicon')
-  async def pedo(self, ctx: discord.Interaction, member: Option(discord.Member, "The people you want to accuse", required=True)):
-      mentionUser = '<@' + str(member.id) + '>'
-      embed = discord.Embed()
-      embed.set_image(url='https://some-random-api.ml/canvas/lolice?avatar=' +
-                      ctx.user.avatar.url)
-      await ctx.response.send_message(f"{mentionUser}", embed=embed)
+    @app_commands.command(name='pedo', description='Expose a lolicon')
+    @app_commands.describe(member="The member to target")
+    async def pedo(self, ctx: discord.Interaction, member: discord.Member = None):
+      try:
+        mentionUser = '<@' + str(member.id) + '>'
+        embed = discord.Embed()
+        embed.set_image(
+            url='https://some-random-api.ml/canvas/lolice?avatar=' +
+            ctx.user.avatar.url)
+        await ctx.response.send_message(f"{mentionUser}", embed=embed)
+      except Exception as error:
+        await ctx.response.send_message("Yang bikin gif anime lg turu")
+        print(error)
 
 async def setup(bot):
-  await bot.add_cog(Pedo(bot))
+    await bot.add_cog(Pedo(bot))

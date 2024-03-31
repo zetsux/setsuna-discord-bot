a11a740402b4906081b74bf63748ad23def75d32
Kevin Nathanael Halim <108170234+zetsux@users.noreply.github.com>
2024-03-31 09:50:50 +0000

    fix: adjust fun commands

--- fun/Diss.py
+++ fun/Diss.py
@@ -2,24 +2,25 @@ import discord
 import random
 from discord.ext import commands
 from discord import app_commands
-from discord.commands import Option
 
-guilds = [990445490401341511]
 
 class Diss(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-  @app_commands.command(name='diss', description='Disses a target')
-  @app_commands.checks.has_any_role('Encoder Magang', 'Owner')
-  async def diss_target(self, ctx: discord.Interaction, member: Option(discord.Member, "Member to diss", required=True)):
-    dissLines = [
-        'Cupu bgt si', 'Diem ya lemah', 'Sini gelud penakut',
-        'Gausah sok jago', 'Janji ga nangis dek?', 'Belajar dulu sana'
-    ]
+    @app_commands.command(name='diss', description='Disses a target')
+    @app_commands.checks.has_any_role('Encoder Magang', 'Owner')
+    @app_commands.describe(member='The member to diss')
+    async def diss_target(self, ctx: discord.Interaction,
+                          member: discord.Member):
+        dissLines = [
+            'Cupu bgt si', 'Diem ya lemah', 'Sini gelud penakut',
+            'Gausah sok jago', 'Janji ga nangis dek?', 'Belajar dulu sana'
+        ]
 
-    dissString = random.choice(dissLines)
-    await ctx.response.send_message(f'Oi {member.name}, {dissString}')
+        dissString = random.choice(dissLines)
+        await ctx.response.send_message(f'Oi {member.name}, {dissString}')
+
 
 async def setup(bot):
-  await bot.add_cog(Diss(bot))
+    await bot.add_cog(Diss(bot))
--- fun/Lyrics.py
+++ fun/Lyrics.py
@@ -3,51 +3,53 @@ import json
 import urllib.request as urllib2
 from discord.ext import commands
 from discord import app_commands
-from discord.commands import Option
 
-guilds = [990445490401341511]
 
 class Lyrics(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-  @app_commands.command(
-      name='lyrics',
-      description='Generate the lyrics of the song title requested')
-  async def lyrics(self, ctx: discord.Interaction, songtitle: Option(str, "Title of the song requested", required=True)):
-      songsearch = ''.join(e for e in songtitle if e.isalnum() or e == ' ')
-      try:
-          response = urllib2.urlopen('https://some-random-api.ml/lyrics?title=' +
-                                     songsearch.replace(' ', '-'))
-          data = json.loads(response.read())
-  
-      except:
-          geniusLink = 'https://genius.com/search?q=' + songsearch.replace(
-              ' ', '-')
-          embed = discord.Embed(
-              title=
-              f"Gomenasai, watashi kurang canggih sepertinya jadi tidak bisa menemukan lagu '{songtitle}' pencarian anata. Ini watashi cariin di web lain deh ya, silahkan dibuka~",
-              description=f"Lyrics : <{geniusLink}>",
-              color=ctx.user.color)
-          await ctx.response.send_message(embed=embed)
-          return
-  
-      if len(data['lyrics']) > 4096:
-          embed = discord.Embed(
-              title=
-              f"Gomenasai, lirik lagu '{songtitle}' yang watashi temuin agak kepanjangan, jadi watashi kasih linknya aja yaa~",
-              description=f"Lyrics : <{data['links']['genius']}>",
-              color=ctx.user.color)
-          await ctx.response.send_message(embed=embed)
-          return
-  
-      embed = discord.Embed(
-          title=f"Lyrics for '{data['title']}' | Requested by {ctx.user.name}",
-          description=data['lyrics'],
-          color=ctx.user.color)
-      embed.set_thumbnail(url=data['thumbnail']['genius'])
-      embed.set_author(name=data['author'])
-      await ctx.response.send_message(embed=embed)
+    @app_commands.command(
+        name='lyrics',
+        description='Generate the lyrics of the song title requested')
+    @app_commands.describe(songtitle='The title of the song')
+    async def lyrics(self, ctx: discord.Interaction, songtitle: str):
+        songsearch = ''.join(e for e in songtitle if e.isalnum() or e == ' ')
+        try:
+            response = urllib2.urlopen(
+                'https://some-random-api.ml/lyrics?title=' +
+                songsearch.replace(' ', '-'))
+            data = json.loads(response.read())
+
+        except:
+            geniusLink = 'https://genius.com/search?q=' + songsearch.replace(
+                ' ', '-')
+            embed = discord.Embed(
+                title=
+                f"Gomenasai, watashi kurang canggih sepertinya jadi tidak bisa menemukan lagu '{songtitle}' pencarian anata. Ini watashi cariin di web lain deh ya, silahkan dibuka~",
+                description=f"Lyrics : <{geniusLink}>",
+                color=ctx.user.color)
+            await ctx.response.send_message(embed=embed)
+            return
+
+        if len(data['lyrics']) > 4096:
+            embed = discord.Embed(
+                title=
+                f"Gomenasai, lirik lagu '{songtitle}' yang watashi temuin agak kepanjangan, jadi watashi kasih linknya aja yaa~",
+                description=f"Lyrics : <{data['links']['genius']}>",
+                color=ctx.user.color)
+            await ctx.response.send_message(embed=embed)
+            return
+
+        embed = discord.Embed(
+            title=
+            f"Lyrics for '{data['title']}' | Requested by {ctx.user.name}",
+            description=data['lyrics'],
+            color=ctx.user.color)
+        embed.set_thumbnail(url=data['thumbnail']['genius'])
+        embed.set_author(name=data['author'])
+        await ctx.response.send_message(embed=embed)
+
 
 async def setup(bot):
-  await bot.add_cog(Lyrics(bot))
+    await bot.add_cog(Lyrics(bot))
